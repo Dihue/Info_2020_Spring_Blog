937661f454bbdb534f6873fd40116beec447a747
Dihue <dihue21@gmail.com>
2020-12-22 12:11:10 -0300

    Conexion con DB y creacion de esqueleto principal

---
+++ myblog/src/main/java/com/informatorio/myblog/models/Usuario.java
@@ -0,0 +1,8 @@
+package com.informatorio.myblog.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Usuario {
+    
+}
---
+++ myblog/src/main/java/com/informatorio/myblog/repositories/CommentRepository.java
@@ -0,0 +1,5 @@
+package com.informatorio.myblog.repositories;
+
+public interface CommentRepository {
+    
+}
