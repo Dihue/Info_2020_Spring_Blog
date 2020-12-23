6be33b347146b6d27ccd41c4c153dbdd02d00101
Dihue <dihue21@gmail.com>
2020-12-23 12:39:02 -0300

    Fix con database

--- myblog/src/main/java/com/informatorio/myblog/models/Post.java
+++ myblog/src/main/java/com/informatorio/myblog/models/Post.java
@@ -1,8 +1,14 @@
 package com.informatorio.myblog.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Post {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private Long id;
 }
